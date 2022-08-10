8b89df1b957ec75ee0f4358653d691b7a8088a7e
Julia.S <der.lavender@gmail.com>
2022-08-10 17:06:24 +0300

    Lesson1_1

---
+++ Lesson1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 1: Напишите программу, которая на вход принимает два числа 
+//и выдаёт, какое число большее, а какое меньшее
+
+int Prompt(string message)
+{
+System.Console.Write(message); // Вывести сообщение
+string readValue = Console.ReadLine(); // Считавает с консоли строку
+int result = int.Parse(readValue); // Преобрадует строку в целое число
+return result; // Возвращает результат
+}
+
+int value1 = Prompt( "Введите первое число ->");
+int value2 = Prompt( "Введите второе число ->");
+if (value1> value2) {
+    Console.WriteLine("Первое значение больше второго");
+}
+else
+ {
+    Console.WriteLine("Второе значение больше первого");
+}
