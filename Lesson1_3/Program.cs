ba21eaaca20de342bdbd61442e687a5830356d96
Julia.S <der.lavender@gmail.com>
2022-08-10 18:16:14 +0300

    Lesson1_3

---
+++ Lesson1_3/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+//4 -> Четное
+
+int Prompt(string message)
+{
+System.Console.Write(message);              // Вывести сообщение
+string readValue = Console.ReadLine();      // Считавает с консоли строку
+int result = int.Parse(readValue);          // Преобразует строку в целое число
+return result;                              // Возвращает результат
+}
+
+int N = Prompt("Введите число ");
+if (N%2 > 0)
+    {
+        Console.WriteLine("Число N не четное");
+    }
+else 
+{
+    Console.WriteLine("Число N четное");
+}
+
